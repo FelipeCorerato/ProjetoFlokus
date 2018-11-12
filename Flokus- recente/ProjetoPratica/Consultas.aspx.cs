c9699f43eaccba602eeb942b2e6d282ad7985faf
FelipeCorerato <fcorerato@gmail.com>
2018-11-12 09:02:26 -0200

    aa

--- Flokus- recente/ProjetoPratica/Consultas.aspx.cs
+++ Flokus- recente/ProjetoPratica/Consultas.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using ProjetoPratica.App_Start;
 using ProjetoPratica;
 
 namespace ProjetoPratica
@@ -17,9 +20,30 @@ namespace ProjetoPratica
 
         protected void btnEnviar_Click(object sender, EventArgs e)
         {
+            //SqlCommand comando;
+
+            //String email = "SELECT * FROM Paciente where cpf='" + "'";
+
+            //String conteudo = emailToString();
+
+            // EmailSender emailSender = new EmailSender("clinicaflokus@gmail.com", "Senha123");
+            //emailSender.sendEmail(email, "Consulta em breve", conteudo);
+
+
+
+
+
             EmailSender emailSender = new EmailSender("clinicaflokus@gmail.com", "Senha123");
 
             emailSender.sendEmail("fcorerato@gmail.com", "Ce tem consulta vei", "dados da consulta");
+        }
+
+        protected String emailToString(String nome_paciente, String nome_medico, String horario, String nome_secretaria)
+        {
+
+            return "Bom dia, Sr(a). " + nome_paciente + "! \n" +
+                   "Este é um lembrete comunicando que está marcada uma consulta sua com o(a) Dr(a). " + nome_medico +
+                   "em dois dias a partir do envio deste " + " às " + horario + "\n" + "Atenciosamente, " + nome_secretaria + "ClinicaFlokus";
         }
 
         protected void btnAgendar_Click(object sender, EventArgs e)
--- Flokus- recente/ProjetoPratica/agendarConsulta.aspx.cs
+++ Flokus- recente/ProjetoPratica/agendarConsulta.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using ProjetoPratica.App_Start;
 
 namespace ProjetoPratica
 {
     public partial class agendarConsulta : System.Web.UI.Page
     {
+        conexaoBD con = new conexaoBD();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -21,7 +27,27 @@ namespace ProjetoPratica
 
         protected void btnAgendar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                String conString = WebConfigurationManager.ConnectionStrings["conexaoBD"].ConnectionString;
+                con.Connection(conString);
+                con.AbrirConexao();
+            }
+            catch (Exception erro)
+            {
+               
+            }
+        }
 
+        protected String dadosConsultaToString(String cpf_paciente)
+        {
+            //String nome_paciente, nome_medico, nome_secretaria, horario;
+            //SqlCommand comando;
+
+
+
+            //return "Bom dia, Sr(a). " + nome_paciente + "! \n" + "Este é um lembrete comunicando que está marcada uma consulta sua com o(a) Dr(a). " + nome_medico + "em dois dias a partir do envio deste " + " às " + horario + "\n" + "Atenciosamente, " + nome_secretaria + "ClinicaFlokus";
+            return "";
         }
     }
 }
